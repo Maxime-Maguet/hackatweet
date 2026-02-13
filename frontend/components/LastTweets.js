d015331588e321cca7aaaabbeb398d62a4abed72
Allan <christoph.allan77120@gmail.com>
2026-02-13 15:06:29 +0100

    commit Lastweet

---
+++ frontend/components/LastTweets.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { tweetBdd } from "../reducers/tweet";
+import { useEffect } from "react";
+
+function LastTweet() {
+  const dispatch = useDispatch();
+  const tweet = useSelector(state => state.user.value);
+  const tweetData = useSelector(state => state.tweet.value);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/tweets/viewTweet")
+      .then(res => res.json())
+      .then(data => {
+        if (data.result) {
+          dispatch(tweetBdd(data.tweets));
+        }
+      });
+  }, []);
+
+  const tweets = tweetData.map((data, i) => {
+    return <TweetModel key={i} {...data} />;
+  });
+  return <div>{tweet}</div>;
+}
+
+export default LastTweet;
